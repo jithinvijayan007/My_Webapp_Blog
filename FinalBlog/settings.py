1def3a56d074658b374e1f1841108f01b21198aa
jithinvijayan007 <jithinvijayan007@gmail.com>
2020-08-22 22:20:05 +0530

    static adjustments

--- FinalBlog/settings.py
+++ FinalBlog/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR,'static')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,9 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    STATIC_DIR,
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

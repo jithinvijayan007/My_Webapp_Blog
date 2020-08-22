3fba2471965d426137980d11f0e6791c5dcd76b5
jithinvijayan007 <jithinvijayan007@gmail.com>
2020-08-22 22:28:29 +0530

    Added django-heroku

--- FinalBlog/settings.py
+++ FinalBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILES_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILES_STORAGE = "storages.backends.s3boto3.s3Boto3Storage"
+
+django_heroku.settings(locals())
